616a853d119821e61450bea1e87b9860e0980ffb
Thang Loi Tran <thangloitran406@gmail.com>
2023-07-17 15:58:03 +0700

    Fix register user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,6 +68,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'avatar' => asset('imgs/avatar/default_avatar.png'),
         ]);
     }
 }
