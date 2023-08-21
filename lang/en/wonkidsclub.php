3a1c1687a4da691a0ea35bcca6f3c3a171bcac7c
Thang Loi Tran <thangloitran406@gmail.com>
2023-08-21 13:26:55 +0700

    Fix won kids -> wonkids

--- lang/en/wonkidsclub.php
+++ lang/en/wonkidsclub.php
@@ -1,14 +1,14 @@
 <?php
 return [
     'title' => 'Wonkidsclub',
-    'about' => "Won Kids Club is a web-based Bible education media content which opens up an exciting Bible playground for Vietnamese children in Bible study groups.  
-    <br>Won Kids Club has created various media content for educational activities for children to learn about Bible characters in each generation through stories, follow-up activities, praise songs, and memory verses.
+    'about' => "Wonkids Club is a web-based Bible education media content which opens up an exciting Bible playground for Vietnamese children in Bible study groups.  
+    <br>Wonkids Club has created various media content for educational activities for children to learn about Bible characters in each generation through stories, follow-up activities, praise songs, and memory verses.
     <br>As a suitable children's educational content for this generation, it can be utilized as a weekly Bible education tool by parents or Sunday school teachers at church for a whole year. We are planning to extend and develop the program for users in local units, denominations, and other countries.",
     'history' => 'History',
     
-    '2016' => 'Formation of Won Kids Club Development Team (6 Korean Missionaries in Vietnam)',
-    '2017' => 'Proposal of Won Kids Club, Beginning of Old Testament Contents',
-    '2019.1' => '4/2019: Preparation for Won Kids Club Webpage ',
+    '2016' => 'Formation of Wonkids Club Development Team (6 Korean Missionaries in Vietnam)',
+    '2017' => 'Proposal of Wonkids Club, Beginning of Old Testament Contents',
+    '2019.1' => '4/2019: Preparation for Wonkids Club Webpage ',
     '2019.2' => '9/2019: Completion of Webpage',
 
     '2020' => 'Opened the 1st Wonkids Club teacher information session (Ho Chi Minh city) (June)',
