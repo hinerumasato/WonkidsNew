7e0eaf3c235830e592311ac1c7243c3985709e11
masato <thangloitran406@gmail.com>
2024-03-25 00:31:45 +0700

    Add store cache

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -32,12 +32,12 @@ class Kernel extends HttpKernel
     protected $middlewareGroups = [
         'web' => [
             \App\Http\Middleware\EncryptCookies::class,
+            \App\Http\Middleware\StoreCache::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\Locale::class,
         ],
 
         'api' => [
--- app/Http/Middleware/StoreCache.php
+++ app/Http/Middleware/StoreCache.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class ClearCache
+class StoreCache
 {
     /**
      * Handle an incoming request.
@@ -14,11 +14,10 @@ class ClearCache
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
         $response = $next($request);
-        return $response->header('Cache-Control','no-cache, no-store, max-age=0, must-revalidate')
-                        ->header('Pragma','no-cache')
-                        ->header('Expires','Sun, 02 Jan 1990 00:00:00 GMT');
+        $response->header('Cache-Control', 'public, max-age=86400');
+        return $response;
     }
 }
