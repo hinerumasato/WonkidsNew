7d653fb8bc4ae2d361b6b0336a4cea99a6605cc3
masato <thangloitran406@gmail.com>
2024-04-05 18:40:32 +0700

    style: change korea font family and responsive cookie consent form

--- resources/views/client/layouts/master.blade.php
+++ resources/views/client/layouts/master.blade.php
@@ -5,21 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="shortcut icon" href="{{ asset('imgs/icon/favicon.ico') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-        rel="stylesheet">
-    <link href="https://db.onlinewebfonts.com/c/3a025ae92e6446cec24efcb6d29e5bf3?family=Malgun+Gothic" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/master/bundle.css?v=') }}{{ env('STATIC_FILE_VERSION') }}">
     
     {{-- CDNS --}}
@@ -45,8 +35,10 @@
             <img src="{{asset('imgs/icon/cookies.png')}}" alt="Cookies Image" width="16px" height="16px">
             <span>@lang('general.cookies-description')</span>
         </p>
-        <button id="cancelCookieBtn">@lang('general.cancel')</button>
-        <button id="acceptCookieBtn">@lang('general.accept-cookies')</button>
+        <div class="button-wrapper">
+            <button id="cancelCookieBtn">@lang('general.cancel')</button>
+            <button id="acceptCookieBtn">@lang('general.accept-cookies')</button>
+        </div>
     </div>
     @include('client.partials.header')
 
